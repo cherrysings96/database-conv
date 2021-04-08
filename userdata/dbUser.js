95b432a06438ad52547b69400f36a5dca500656c
cherrysing96 <sherene.antony@gmail.com>
2021-04-08 20:16:28 +0530

    reverting to stable state

---
+++ userdata/dbUser.js
@@ -0,0 +1,12 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "hello",
+  host: "localhost",
+  port: 5432,
+  database: "postgres"
+});
+
+
+module.exports = pool;
