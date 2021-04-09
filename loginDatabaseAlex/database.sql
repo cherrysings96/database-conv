81a82477e49fe696374ee7211b2dd0c24a0d0691
ALEX THOMAS <macbook@ALEXs-MacBook-Air.local>
2021-04-09 11:51:12 +0530

    merged login and todo backed together

--- loginDatabaseAlex/database.sql
+++ loginDatabaseAlex/database.sql
@@ -7,4 +7,12 @@ CREATE TABLE users(
   user_password VARCHAR(255) NOT NULL
  );
 
-INSERT INTO users ( user_email, user_password) VALUES ('abc@gmail.com', 'abc');
+INSERT INTO userdata ( id, fullname, email, password) VALUES (20, 'admin', 'a@mail.com', '123');
+
+-- //sherene table 
+CREATE TABLE userdata(
+  id INTEGER PRIMARY KEY,
+  fullname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
