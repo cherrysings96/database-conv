6c601052ccf7817643e2c87fdd17b4a86550ed5b
cherrysing96 <sherene.antony@gmail.com>
2021-04-09 11:28:19 +0530

    userdata database

--- loginDatabaseAlex/database.sql
+++ loginDatabaseAlex/database.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE jwttutorial;
+CREATE DATABASE postgres;
 
-CREATE TABLE users(
-  user_id SERIAL PRIMARY KEY,
+CREATE TABLE userdata(
+  id SERIAL PRIMARY KEY,
   fullname VARCHAR(255) NOT NULL,
-  user_email VARCHAR(255) NOT NULL UNIQUE,
-  user_password VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
  );
 
-INSERT INTO users ( user_email, user_password) VALUES ('abc@gmail.com', 'abc');
+INSERT INTO userdata (email, password) VALUES ('abc@gmail.com', 'abc');
