82205beb5320882d8b779a67b580715b7de6d264
ALEX THOMAS <macbook@ALEXs-MacBook-Air.local>
2021-04-15 13:17:49 +0530

    sql commands for database and table creation

--- loginDatabase/database.sql
+++ loginDatabase/database.sql
@@ -1,5 +1,14 @@
-CREATE DATABASE postgres;
+CREATE DATABASE authtodo;
 
+--//Alex Table Todo
+CREATE TABLE todo(
+  todo_id SERIAL PRIMARY KEY,
+  description VARCHAR(255) NOT NULL
+ );
+
+INSERT INTO todo ( todo_id,description) VALUES (1,'Attend Evening Meeting');
+
+--//UserSherene & LoginAlex common table
 CREATE TABLE userdata(
   id SERIAL PRIMARY KEY,
   fullname VARCHAR(255) NOT NULL,
@@ -7,17 +16,9 @@ CREATE TABLE userdata(
   password VARCHAR(255) NOT NULL
  );
 
-
-INSERT INTO userdata (email, password) VALUES ('abc@gmail.com', 'abc');
 INSERT INTO userdata ( id, fullname, email, password) VALUES (20, 'admin', 'a@mail.com', '123');
 
--- //sherene table 
-CREATE TABLE userdata(
-  id INTEGER PRIMARY KEY,
-  fullname VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL
-);
+
  -- //Jasim table
 CREATE TABLE merchdata (
   id INTEGER PRIMARY KEY,
